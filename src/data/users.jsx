965f59c37d45f966aaea10569d3d84ae9a67a159
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-29 23:48:33 +0530

    feat: add showcase page

---
+++ src/data/users.jsx
@@ -0,0 +1,34 @@
+export const users = [
+  {
+    title: "AgileTs",
+    description: "Global State and Logic Framework for reactive Applications",
+    preview: require("./showcase/agilets.png"),
+    website: "https://agile-ts.org/",
+    source: "https://github.com/agile-ts/documentation",
+    tags: ["opensource", "design"],
+  },
+  {
+    title: "AgileTs",
+    description: "Global State and Logic Framework for reactive Applications",
+    preview: require("./showcase/agilets.png"),
+    website: "https://agile-ts.org/",
+    source: "https://github.com/agile-ts/documentation",
+    tags: ["opensource", "design"],
+  },
+  {
+    title: "AgileTs",
+    description: "Global State and Logic Framework for reactive Applications",
+    preview: require("./showcase/agilets.png"),
+    website: "https://agile-ts.org/",
+    source: "https://github.com/agile-ts/documentation",
+    tags: ["opensource", "design"],
+  },
+  {
+    title: "AgileTs",
+    description: "Global State and Logic Framework for reactive Applications",
+    preview: require("./showcase/agilets.png"),
+    website: "https://agile-ts.org/",
+    source: "https://github.com/agile-ts/documentation",
+    tags: ["opensource", "design"],
+  },
+];
---
+++ src/pages/showcase/index.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Translate, { translate } from "@docusaurus/Translate";
+import Layout from "@theme/Layout";
+import { users } from "../../data/users";
+import ShowcaseCard from "../../components/ShowcaseCard/ShowcaseCard";
+import "./showcase.css";
+import FilterChip from "../../components/FilterChip/FilterChip";
+
+const filters = [
+  {
+    id: 1,
+    name: "Favourite",
+    color: "pink",
+  },
+  {
+    id: 1,
+    name: "Open Source",
+    color: "red",
+  },
+  {
+    id: 1,
+    name: "Product",
+    color: "green",
+  },
+  {
+    id: 1,
+    name: "Design",
+    color: "blue",
+  },
+];
+
+const ShowcaseHeader = () => {
+  return (
+    <section className="margin-top--lg margin-bottom--lg text--center">
+      <h1>Mockbee Showcase</h1>
+      <p>List of projetcs people are building with Mockbee</p>
+      <a
+        className="button button--primary"
+        href="#"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Translate id="showcase.header.button">
+          🙏 Please add your site
+        </Translate>
+      </a>
+    </section>
+  );
+};
+
+function ShowcaseFilters() {
+  return (
+    <section className="container margin-top--l margin-bottom--lg">
+      <div className="margin-bottom--sm">
+        <div>
+          <h2>
+            <Translate id="showcase.filters.title">Filters</Translate>
+          </h2>
+        </div>
+      </div>
+      <ul className="clean-list filter-container">
+        {filters.map((filter) => (
+          <FilterChip key={filter.id} filter={filter} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function ShowcaseCards() {
+  return (
+    <div className="showcase-cards-container container margin-top--l margin-bottom--lg">
+      {users.map((user) => (
+        <ShowcaseCard user={user} />
+      ))}
+    </div>
+  );
+}
+
+export default function Showcase() {
+  return (
+    <Layout>
+      <ShowcaseHeader />
+      <ShowcaseFilters />
+      <ShowcaseCards />
+    </Layout>
+  );
+}
